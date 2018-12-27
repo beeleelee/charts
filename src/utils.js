d4dd5586954d1fff10529c0b1b1d115d131ff323
lifeng <beeleelee@outlook.com>
2018-12-27 09:51:39 +0800

    path line

--- src/utils.js
+++ src/utils.js
@@ -4,3 +4,16 @@ function pathData(drawFunc, data) {
   })
   return drawFunc()
 }
+
+var PATH = {}
+
+PATH.line = (points) => {
+  let len = points.length
+  let data = []
+  let index, point
+  for (index = 0; index < len; index++) {
+    point = points[index]
+    data.push(`${point[0]},${point[1]}`)
+  }
+  return `M${data.join('L')}`
+}
