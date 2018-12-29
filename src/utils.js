da290be3d32bf1a4321ff8933b301c3abb143ad3
lifeng <beeleelee@outlook.com>
2018-12-29 10:18:06 +0800

    draw arc sector

--- src/utils.js
+++ src/utils.js
@@ -24,20 +24,30 @@ PATH.arc = ({
   startAngle,
   endAngle
 }) => {
-  let angle2Degree = 180 / Math.PI
-  // startAngle -= 90 * angle2Degree
-  // endAngle -= 90 * angle2Degree
-  let startPoint = { x: 0, y: 0 }
-  let firstPoint = {
+  let angle2Degree = Math.PI / 180
+  startAngle -= 90 * angle2Degree
+  endAngle -= 90 * angle2Degree
+  let outerArcStart = {
     x: outerRadius * Math.cos(startAngle),
     y: outerRadius * Math.sin(startAngle)
   }
-  let secondPoint = {
+  let outerArcEnd = {
     x: outerRadius * Math.cos(endAngle),
     y: outerRadius * Math.sin(endAngle)
   }
-  console.log(firstPoint, secondPoint)
-  let largeArc = (endAngle - startAngle) * angle2Degree > 180 ? 1 : 0
+  let largeArc = (endAngle - startAngle) / angle2Degree > 180 ? 1 : 0
+  let innerArcStart, innerArcEnd
+  if (innerRadius > 0) {
+    innerArcStart = {
+      x: innerRadius * Math.cos(startAngle),
+      y: innerRadius * Math.sin(startAngle)
+    }
+    innerArcEnd = {
+      x: innerRadius * Math.cos(endAngle),
+      y: innerRadius * Math.sin(endAngle)
+    }
+    return `M${outerArcStart.x},${outerArcStart.y}A${outerRadius},${outerRadius} 0 ${largeArc},1 ${outerArcEnd.x},${outerArcEnd.y}L${innerArcEnd.x},${innerArcEnd.y}A${innerRadius},${innerRadius} 0 ${largeArc},0 ${innerArcStart.x},${innerArcStart.y}Z`
+  }
 
-  return `M0,0L${firstPoint.x},${firstPoint.y}A${outerRadius},${outerRadius} 0 ${largeArc},1 ${secondPoint.x},${secondPoint.y}Z`
+  return `M0,0L${outerArcStart.x},${outerArcStart.y}A${outerRadius},${outerRadius} 0 ${largeArc},1 ${outerArcEnd.x},${outerArcEnd.y}Z`
 }
