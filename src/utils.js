d36d1300155c4b911a90156096c0677e320f0353
lifeng <beeleelee@outlook.com>
2018-12-29 08:58:04 +0800

    arc path

--- src/utils.js
+++ src/utils.js
@@ -25,16 +25,19 @@ PATH.arc = ({
   endAngle
 }) => {
   let angle2Degree = 180 / Math.PI
+  // startAngle -= 90 * angle2Degree
+  // endAngle -= 90 * angle2Degree
   let startPoint = { x: 0, y: 0 }
   let firstPoint = {
     x: outerRadius * Math.cos(startAngle),
-    y: -outerRadius * Math.sin(startAngle)
+    y: outerRadius * Math.sin(startAngle)
   }
   let secondPoint = {
-    x: outerRadius * Math.cos(startAngle + endAngle),
-    y: -outerRadius * Math.sin(startAngle + endAngle)
+    x: outerRadius * Math.cos(endAngle),
+    y: outerRadius * Math.sin(endAngle)
   }
+  console.log(firstPoint, secondPoint)
   let largeArc = (endAngle - startAngle) * angle2Degree > 180 ? 1 : 0
 
-  return `M0,0L${firstPoint.x},${firstPoint.y}A${outerRadius},${outerRadius} 0 ${largeArc},0 ${secondPoint.x},${secondPoint.y}Z`
+  return `M0,0L${firstPoint.x},${firstPoint.y}A${outerRadius},${outerRadius} 0 ${largeArc},1 ${secondPoint.x},${secondPoint.y}Z`
 }
